cf89649c27bac3f9bf224ae1adcb386b9699bb1d
pawidachum <pawida.may@gmail.com>
2021-03-16 22:51:38 +0700

    ajax post detail

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  posts: any[];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .subscribe(result => {
+      this.posts = result as any[];
+    })
+   }
+
+}
